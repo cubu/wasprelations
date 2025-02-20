c92784c2fa921c350681a707567384733b74cafe
Genyus <genyus@gmail.com>
2025-02-20 16:26:23 +0000

    fix: fixes operation declarations
    - Uses correct operations
    - Removes redundant entities

--- src/api/v1/modules/tags/queries/getTagsForTask.ts
+++ src/api/v1/modules/tags/queries/getTagsForTask.ts
@@ -1,11 +1,11 @@
 import { Tag } from 'wasp/entities'  // Import Tag entity
-import { type GetTasks } from 'wasp/server/operations'
+import { GetTagsForTask } from 'wasp/server/operations'
 
 type GetTagsForTaskPayload = {
   taskId: string
 }
 
-export const getTagsForTask: GetTasks<GetTagsForTaskPayload, Tag[]> = async ({ taskId }, context) => {
+export const getTagsForTask: GetTagsForTask<GetTagsForTaskPayload, Tag[]> = async ({ taskId }, context) => {
   // Get the task by taskId first
   const task = await context.entities.Task.findUnique({
     where: { id: taskId },
--- src/api/v1/modules/tasks/queries/getFilteredTasks.ts
+++ src/api/v1/modules/tasks/queries/getFilteredTasks.ts
@@ -1,12 +1,12 @@
 import { Task } from 'wasp/entities'
-import { type GetTasks } from 'wasp/server/operations'
+import { GetFilteredTasks } from 'wasp/server/operations'
 
 // Define the type for the query arguments
 type GetFilteredTasksArgs = {
   isDone?: boolean  // This will filter tasks based on whether they are done or not
 }
 
-export const getFilteredTasks: GetTasks<GetFilteredTasksArgs, Task[]> = async (
+export const getFilteredTasks: GetFilteredTasks<GetFilteredTasksArgs, Task[]> = async (
   args,
   context
 ) => {
